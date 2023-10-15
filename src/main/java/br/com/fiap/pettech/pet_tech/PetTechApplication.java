8ebd3a06d917b65d4e2dfbf23eb703de86de0d06
Luiz Guilherme Costa Pimentel <80006705+LzGui@users.noreply.github.com>
2023-10-15 09:53:45 -0300

    criando o CRUD

---
+++ src/main/java/br/com/fiap/pettech/pet_tech/PetTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pettech.pet_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //indica para o spring a classe principal
+public class PetTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTechApplication.class, args);
+	}
+
+}
